97d9f7fffbc86d07008683a7c72370a910c16340
Justin Solo <justinpaulosolo@gmail.com>
2023-08-31 02:25:12 -0700

    init

---
+++ P1480RunningSumOf1dArray.cs
@@ -0,0 +1,22 @@
+namespace LeetCode;
+
+public class P1480RunningSumOf1dArray
+{
+    private static int[] RunningSum(int[] nums)
+    {
+        var total = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            total += nums[i];
+            nums[i] = total;
+        }
+        return nums;
+    }
+    
+    [Theory]
+    [InlineData(new[] {1,2,3,4}, new[] {1,3,6,10})]
+    [InlineData(new[] {1,1,1,1,1}, new[] {1,2,3,4,5})]
+    public void Test(int[] nums, int[] expected) {
+        Assert.Equal(expected, RunningSum(nums));
+    }
+}
