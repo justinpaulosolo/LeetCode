51ad284b7fb48867d9eed6c9cfdaa29b71344db5
Justin Solo <justinpaulosolo@gmail.com>
2023-08-31 04:41:57 -0700

    anagrams

---
+++ P1929ConcatenationofArray.cs
@@ -0,0 +1,25 @@
+namespace LeetCode;
+
+public class P1929ConcatenationofArray
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        var arr = new int[nums.Length * 2];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            arr[i] = nums[i];
+            arr[i + nums.Length] = nums[i];
+        }
+
+        return arr;
+    }
+    
+    [Theory]
+    [InlineData(new []{1,2,1}, new[]{1,2,1,1,2,1})]
+    [InlineData(new []{1,3,2,1}, new[]{1,3,2,1,1,3,2,1})]
+    public void Test(int[] nums, int[] expected)
+    {
+        Assert.Equal(GetConcatenation(nums),expected);
+    }
+}
