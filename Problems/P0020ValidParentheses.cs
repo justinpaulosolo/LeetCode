2d81e204581bc6b6c4b9cfd99b18877f01542e41
Justin Solo <justinpaulosolo@gmail.com>
2023-09-05 21:45:22 -0700

    unsolved P0020

--- Problems/P0020ValidParentheses.cs
+++ Problems/P0020ValidParentheses.cs
@@ -8,8 +8,36 @@ namespace LeetCode.Problems
 {
     public class P0020ValidParentheses
     {
-        public void Test()
-        { }
+        public bool IsValid(string s)
+        {
+            var map = new Dictionary<char, int>()
+            {
+                {'(',0 },
+                {')',0 },
+                {'{',0 },
+                {'}',0 },
+                {'[',0 },
+                {']',0 },
+            };
+
+            foreach (var item in s)
+            {
+                map[item]++;
+            }
+            int firstValue = map.First().Value;
+            bool allValuesSame = map.All(x => x.Value == firstValue);
+
+            return allValuesSame;
+        }
+
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        public void Test(string s, bool expected)
+        { 
+            Assert.Equal(IsValid(s), expected);
+        }
     }
 
     
